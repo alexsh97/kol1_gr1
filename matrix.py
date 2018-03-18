b50016155222062dd84db8ca2e2be3f5bdfdadd7
alexsh97 <alexsh997@gmail.com>
2018-03-18 17:38:27 +0100

    Kol1_version 2.0

---
+++ matrix.py
@@ -0,0 +1,98 @@
+class Matrix:
+
+	def __init__(self,*rows):
+		for row in rows:
+			if not isinstance(row, list):
+				raise TypeError("It is not a row for matrix")
+			if len(row) != len(rows[0]):
+				raise TypeError("Size of columns is not the same")
+			for element in row:
+				if not isinstance(element, (int, float)):
+					raise TypeError("Element '"+str(element)+"' is not a number")
+		
+		self.matrix = [] 
+		for row in rows:
+			self.matrix.append(row) 
+		self.rows = len(self.matrix)
+		self.columns = len(self.matrix[0])
+		
+
+	def myGenerator(self):
+		for row in self.matrix:
+			yield row
+	
+	
+		
+	def __str__(self):
+		result = "Matrix:" + str(self.rows) + "x" + str(self.columns) + "\n"
+		for row in self.myGenerator():
+			result += str(row) + "\n"
+		return result
+
+
+	def plus(self,matrix2):
+		if self.rows == matrix2.rows and self.columns == matrix2.columns:
+			print("add matrix2")
+			new_matrix = []
+			for row in range(0,self.rows):
+				new_matrix.append([])
+				for column in range(0,self.columns):
+					new_matrix[row].append(self.matrix[row][column] + matrix2.matrix[row][column])	
+			return Matrix(*new_matrix)
+		else:
+			raise TypeError("Sizes of matrixs are not the same")
+
+
+	def __add__(self, rightObject):
+		if  isinstance(rightObject, (int,float)):
+			new_matrix = []
+			for row in range(0,self.rows):
+				new_matrix.append([])
+				for column in range(0,self.columns):
+					new_matrix[row].append(self.matrix[row][column] + rightObject)
+			return Matrix(*new_matrix)
+		elif isinstance(rightObject,Matrix): 
+			return self.plus(rightObject)
+		else:
+			raise TypeError("Element '" + str(rightObject) + "' is not a number")
+
+
+	def mul(self,matrix2):
+		new_matrix = []
+		for i in range(0,self.rows):
+			new_matrix.append([])
+			for j in range(0,matrix2.columns):
+				new_element = [self.matrix[i][k] * matrix2.matrix[k][j] for k in range(0,self.columns)]				
+				new_matrix[i].append(sum(new_element))
+				
+		return Matrix(*new_matrix)		
+
+
+	def __mul__(self,rightObject):
+
+		if  isinstance(rightObject, (int,float)):
+			new_matrix = []
+			for row in range(0,self.rows):
+				new_matrix.append([])
+				for column in range(0,self.columns):
+					new_matrix[row].append(self.matrix[row][column] * rightObject)
+			return Matrix(*new_matrix)
+		elif isinstance(rightObject,Matrix): 
+			if self.columns == rightObject.rows:
+				return self.mul(rightObject)
+			else:
+				raise TypeError("Number of columns of left matrix equal to the number of rows of right matrix ")
+		else:
+			raise TypeError("Element '" + str(rightObject) + "' is not a number")
+				
+			
+			
+def function():
+	print("__name__=='__main__'. This should not be printed")
+
+if __name__ == '__main__':
+	function()			
+
+	
+		
+		
--- task.py
+++ task.py
@@ -1,40 +1,33 @@
+from matrix import Matrix
 
-class Matrix22:
-    
-    matrix = [0,0,0,0]
-    
-    def __init__(self,m1,m2,m3,m4):
-            self.matrix = [m1,m2,m3,m4]
-    
-    def sum(self):
-        suma = 0
-        for m in self.matrix:
-            suma = suma + m
-        return suma
-    
-    
-    def prod(self, mat2):
-        #print(str(self.matrix[0]))
-        m1 = (self.matrix[0]*mat2.matrix[0]) + (self.matrix[1]*mat2.matrix[2])
-        m3 = (self.matrix[2]*mat2.matrix[0]) + (self.matrix[3]*mat2.matrix[2])
-        m2 = (self.matrix[0]*mat2.matrix[1]) + (self.matrix[1]*mat2.matrix[3])
-        m4 = (self.matrix[2]*mat2.matrix[1]) + (self.matrix[3]*mat2.matrix[3])
-        mat12 = Matrix22(m1,m2,m3,m4)
-        return mat12
-    def show(self):
-        print("["+str(self.matrix[0]) + " " + str(self.matrix[1])+"]")
-        print("["+str(self.matrix[2]) + " " + str(self.matrix[3])+"]")
-        pass
-        
-   
+if __name__=="__main__":
+
+	print("matrix1")
+	matrix1 = Matrix(
+			[1,2,3],
+			[3,3,3], 
+			[5,4,2]
+			)
+	print(matrix1)
+
+	print("matrix1 + 1 = new_matrix1")
+	new_matrix1 = matrix1 + 1
+	print(new_matrix1)
+
+	print("matrix2")
+	matrix2 = Matrix(
+			[6,7,8],
+			[2,3,1], 
+			[2,8,9]
+			)
+	print(matrix2)
+
+	print("matrix1 + matrix2")
+	new_matrix2 = matrix1 + matrix2
+	print(new_matrix2)
+
+	print("matrix1 * matrix2 = matrix3")
+	matrix3 = matrix1*matrix2
+	print(matrix3)
 
 
-matrix_1  = Matrix22(1,2,3,4)    
-matrix_2 = Matrix22(2,2,2,1)
-
-matrix_3 = matrix_1.prod(matrix_2)
-mat1_sum = matrix_3.sum()
-print ("matrix_3: sum = " + str(mat1_sum))
-
-print ("Matrix3:")
-matrix_3.show()
